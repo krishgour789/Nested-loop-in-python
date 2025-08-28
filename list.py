f6874ef237afffb9f5bc7dc7ac26f86fd8fda805
Krish Gour <krishgour789@gmail.com>
2025-08-28 11:05:17 -0700

    List/Tuple/set/dictionary

---
+++ list.py
@@ -0,0 +1,57 @@
+# What is a List?
+# A list is an ordered, mutable (changeable) collection of items.
+# Can store mixed data types (int, str, float, etc.).
+# Written inside square brackets [ ]
+# my_list = [1, 2, 3, "hello", 4.5]
+# print(my_list)  
+# Output: [1, 2, 3, 'hello', 4.5]
+
+
+# empty_list = []
+# number = [1,2,3,4,5]
+
+
+
+# Accesing elements
+# Indexing starts at 0
+# Negative index --> counts from end
+# nums = [10, 20, 30, 40, 50]
+# print(nums[0])   # 10
+# print(nums[-1])  # 50
+
+
+# slicing
+# nums = [1, 2, 3, 4, 5, 6]
+# print(nums[1:4])   # [2, 3, 4]
+# print(nums[:3])    # [1, 2, 3]
+# print(nums[::2])   # [1, 3, 5]  (step of 2)
+
+# a = ["Python","HTML", "Css"]
+# a[-1]="Python"
+# print(a)
+
+
+# sum of all numbers
+
+# a = [12,3,43,43,23,43]
+# b = sum(a)
+# print(b)
+
+
+# tuples i python ??
+# What is a Tuple?
+# A tuple is an ordered collection like a list.
+# Immutable → you cannot change, add, or remove elements once created.
+# Defined using parentheses ( )
+
+# a = ("krish","gour")
+# a[-1]="krish"
+# print(a)
+
+
+# a = (1,1,2,3,4,5,6,7)
+# print(a.count(1)) with the help of count in tuple you can count the numbers how many times the value print
+
+
+# a = (1,2,3,4,4,5)
+# print(a.index(5)) index is used to know find the indexing value
